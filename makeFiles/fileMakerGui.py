5dfcf6cbe375dcd157e5cc805c664423f4a81ca1
John Paul Antonovich <39472411+tacos247@users.noreply.github.com>
2018-05-21 17:02:33 -0700

    Update fileMakerGui.py

--- makeFiles/fileMakerGui.py
+++ makeFiles/fileMakerGui.py
@@ -4,7 +4,7 @@ import tkinter
 from tkinter import messagebox
 import tkinter as tk
 import random
-from fileMaker import fileMaker
+from makeFiles import MakeFile
 
 docDate = str(datetime.today().strftime('%Y-%m-%d')) # using 'str' will increase app performance
 
